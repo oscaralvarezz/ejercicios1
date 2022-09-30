6acd57d79ea31ee983a87023c3038ec06169c2f9
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-09-30 12:10:58 +0200

    hacemos ejercicio 1
    invertimos la cadena y posteriormente printamos el contenido de ella en el orden que deseamos

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,7 @@
-print ("Hola Mundo")
-print ("Hacemos ejercicio1")
+'''Tenemos que formatear la cadena para posteriormente invertirla'''
+from email.headerregistry import ContentTransferEncodingHeader
+
+
+cadena_nombre = "zeréP nauJ, 01"
+cadena_definitiva = cadena_nombre[::-1].split(",")
+print(cadena_definitiva[1],"ha sacado un:", cadena_definitiva[0], "de nota")
